adf2b5e2e270c634113716c110552313f36da236
Winston Lu <mobcraft120@gmail.com>
2015-12-21 14:54:29 -0600

    Finalized Countdown

--- bomb-timer/bomb-timer.cpp
+++ bomb-timer/bomb-timer.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <chrono> //Used for time
 #include <thread> //Also used for time
+#include <stdlib.h> //For random score
 using namespace std; //Saves time
 int main(){
+int scorea, scoreb;
+scorea = rand() % 20 + 1;
+scoreb = rand() % 20 + 1;
 cout << "BOMB COUNTDOWN" << endl;
+this_thread::sleep_for(chrono::second(2));
 for (int bombtimer=10; /*Creates bomb timer variable*/bombtimer>-1; /*Checks if bomb timer is bigger than -1*/--bombtimer/*subtracts one from bomb timer*/) {
 cout << bombtimer << endl;
 this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
 }
 cout << "BOOM" << endl;
+this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
+cout << "Player had been killed. Score: " << scorea << "-" << scoreb << endl;
 return 0; //Exits application
 }
