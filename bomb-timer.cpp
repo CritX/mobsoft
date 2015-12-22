f52d000a7ca3934ffa7e322620ed263cf1c77756
Winston Lu <mobcraft120@gmail.com>
2015-12-21 22:30:32 -0600

    Update bomb-timer.cpp

--- bomb-timer.cpp
+++ bomb-timer.cpp
@@ -2,19 +2,18 @@
 #include <chrono> //Used for time
 #include <thread> //Also used for time
 #include <stdlib.h> //For random score
-using namespace std; //Saves time
 int main(){
 int scorea, scoreb;
 scorea = rand() % 20 + 1; //1 is min, 20 is max
 scoreb = rand() % 20 + 1; //Same thing here
-cout << "BOMB COUNTDOWN" << endl;
-this_thread::sleep_for(chrono::second(2)); //Gives time for Bomb Countdown text
+std::cout << "BOMB COUNTDOWN" << endl;
+std::this_thread::sleep_for(chrono::second(2)); //Gives time for Bomb Countdown text
 for (int bombtimer=10; /*Creates bomb timer variable*/bombtimer>-1; /*Checks if bomb timer is bigger than -1*/--bombtimer/*subtracts one from bomb timer*/) {
-cout << bombtimer << endl;
-this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
+std::cout << bombtimer << endl;
+std::this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
 }
-cout << "BOOM" << endl;
-this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
-cout << "Player194 had been killed. Score: " << scorea << "-" << scoreb << endl;
+std::cout << "BOOM" << endl;
+std::this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
+std::cout << "Player194 had been killed. Score: " << scorea << "-" << scoreb << endl;
 return 0; //Exits application
 }
