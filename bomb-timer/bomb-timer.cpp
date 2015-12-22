66bed914539a0dfbd86261409d619ee3d0b5c855
Winston Lu <mobcraft120@gmail.com>
2015-12-21 20:05:24 -0600

    Update bomb-timer.cpp

--- bomb-timer/bomb-timer.cpp
+++ bomb-timer/bomb-timer.cpp
@@ -5,8 +5,8 @@
 using namespace std; //Saves time
 int main(){
 int scorea, scoreb;
-scorea = rand() % 20 + 1;
-scoreb = rand() % 20 + 1;
+scorea = rand() % 20 + 1; //1 is min, 20 is max
+scoreb = rand() % 20 + 1; //Same thing here
 cout << "BOMB COUNTDOWN" << endl;
 this_thread::sleep_for(chrono::second(2)); //Gives time for Bomb Countdown text
 for (int bombtimer=10; /*Creates bomb timer variable*/bombtimer>-1; /*Checks if bomb timer is bigger than -1*/--bombtimer/*subtracts one from bomb timer*/) {
