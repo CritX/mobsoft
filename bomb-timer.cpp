5e059423e7cf7e5f0d2dfecca995bcafd1728a09
Winston Lu <mobcraft120@gmail.com>
2015-12-22 10:48:19 -0600

    Update bomb-timer.cpp

--- bomb-timer.cpp
+++ bomb-timer.cpp
@@ -2,7 +2,7 @@
 #include <chrono> //Used for time
 #include <thread> //Also used for time
 #include <stdlib.h> //For random score
-void main(){
+int main(int argc, char *argv[]){
 int scorea, scoreb;
 scorea = rand() % 20 + 1; //1 is min, 20 is max
 scoreb = rand() % 20 + 1; //Same thing here
