c20a6420d598ba56806d89df90fe85763450398e
Winston Lu <mobcraft120@gmail.com>
2015-12-21 19:59:26 -0600

    Update bomb-timer.cpp

--- bomb-timer/bomb-timer.cpp
+++ bomb-timer/bomb-timer.cpp
@@ -8,7 +8,7 @@ int scorea, scoreb;
 scorea = rand() % 20 + 1;
 scoreb = rand() % 20 + 1;
 cout << "BOMB COUNTDOWN" << endl;
-this_thread::sleep_for(chrono::second(2));
+this_thread::sleep_for(chrono::second(2)); //Gives time for Bomb Countdown text
 for (int bombtimer=10; /*Creates bomb timer variable*/bombtimer>-1; /*Checks if bomb timer is bigger than -1*/--bombtimer/*subtracts one from bomb timer*/) {
 cout << bombtimer << endl;
 this_thread::sleep_for(chrono::seconds(1)); //Wait 1 second
